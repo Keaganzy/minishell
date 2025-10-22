88741ff89ba7b30f120e79fbc2ef6daa857f6cca
Joanne Yulin Tong <jotong@c1r4s6.42singapore.sg>
2025-10-22 16:42:58 +0800

    support ignore sigquit

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: jotong <jotong@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 22:52:55 by jotong            #+#    #+#             */
-/*   Updated: 2025/10/15 14:54:27 by jotong           ###   ########.fr       */
+/*   Updated: 2025/10/22 16:41:59 by jotong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,8 @@ void	sigint_handler(int sig)
 	rl_redisplay();
 }
 
-void	sigquit_handler(int sig)
-{
-	(void)sig;
-	// write(1, "\b\b \b\b", 6);
-}
-
 void	set_signals(void)
 {
 	signal(SIGINT, sigint_handler);		// ctrl-C
-	signal(SIGQUIT, sigquit_handler);	// ctrl-"\"
+	signal(SIGQUIT, SIG_IGN); // ctrl-"\"
 }
