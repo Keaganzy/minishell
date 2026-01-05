0dc1a2a6c0f340dd3214d5035a6b7ea8a9c0988f
Joanne T <cheggmate@gmail.com>
2026-01-05 16:19:16 +0000

    fix memleak

--- src/lexer/lexer_utils3.c
+++ src/lexer/lexer_utils3.c
@@ -6,25 +6,63 @@
 /*   By: jotong <jotong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 15:25:56 by jotong            #+#    #+#             */
-/*   Updated: 2026/01/05 14:32:31 by jotong           ###   ########.fr       */
+/*   Updated: 2026/01/05 16:18:54 by jotong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
+// int	parse_quotes(char **wrd, char *s, t_token **tokens, size_t *i)
+// {
+// 	if (s[*i] != '"' && s[*i] != '\'')
+// 		return (0);
+// 	if (*wrd)
+// 		*wrd = ft_strjoin_and_free(wrd, extract_word_with_inv_commas(s, i), 3);
+// 	else
+// 		*wrd = extract_word_with_inv_commas(s, i);
+// 	if (!(s[*i] == '\0' || s[*i] == ' '))
+// 	{
+// 		return (1);
+// 	}
+// 	add_token_back(tokens, token_new(T_WORD, *wrd, -1));
+// 	free(*wrd);
+// 	*wrd = NULL;
+// 	return (1);
+// }
+
+// int	parse_quotes(char **wrd, char *s, t_token **tokens, size_t *i)
+// {
+// 	char	*extracted;
+
+// 	if (s[*i] != '"' && s[*i] != '\'')
+// 		return (0);
+// 	extracted = extract_word_with_inv_commas(s, i);
+// 	if (*wrd)
+// 		*wrd = ft_strjoin_and_free(wrd, extracted, 3);
+// 	else
+// 		*wrd = extracted;
+// 	if (!(s[*i] == '\0' || s[*i] == ' '))
+// 		return (1);
+// 	add_token_back(tokens, token_new(T_WORD, *wrd, -1));
+// 	free(*wrd);
+// 	*wrd = NULL;
+// 	return (1);
+// }
+
 int	parse_quotes(char **wrd, char *s, t_token **tokens, size_t *i)
 {
+	char	*extracted;
+
 	if (s[*i] != '"' && s[*i] != '\'')
 		return (0);
+	extracted = extract_word_with_inv_commas(s, i);
 	if (*wrd)
-		*wrd = ft_strjoin_and_free(wrd, extract_word_with_inv_commas(s, i), 3);
+		*wrd = ft_strjoin_and_free(wrd, extracted, 3);
 	else
-		*wrd = extract_word_with_inv_commas(s, i);
+		*wrd = extracted;
 	if (!(s[*i] == '\0' || s[*i] == ' '))
-	{
 		return (1);
-	}
 	add_token_back(tokens, token_new(T_WORD, *wrd, -1));
 	free(*wrd);
 	*wrd = NULL;
@@ -35,7 +73,7 @@ int	parse_word(char **word, char *s, t_token **tokens, size_t *i)
 {
 	if (s[*i] == '\0')
 		return (0);
-	*word = ft_strjoin_and_free(word, extract_word(s, i), 2);
+	*word = ft_strjoin_and_free(word, extract_word(s, i), 3);
 	if (s[*i] == ' ' || s[*i] == '\0' || s[*i] == '<' || s[*i] == '>'
 		|| s[*i] == '(' || s[*i] == ')' || s[*i] == '&' || s[*i] == '|')
 	{
