ca5889e82dada43c94f50f1b4da55dc0a6d35ea8
Joanne Yulin Tong <jotong@c1r2s2.42singapore.sg>
2026-01-07 22:20:34 +0800

    fixed

--- src/parser/parser_cmd_redirect_new.c
+++ src/parser/parser_cmd_redirect_new.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_cmd_redirect_new.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jotong <jotong@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jotong <jotong@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 16:54:39 by ksng              #+#    #+#             */
-/*   Updated: 2026/01/05 14:48:44 by jotong           ###   ########.fr       */
+/*   Updated: 2026/01/07 22:11:36 by jotong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,41 +99,147 @@ char	*strip_quotes(const char *str, int *flag)
 	return (result);
 }
 
+// t_ast *parse_one_redir(t_parser *p, t_ast *cmd, t_shell *shell)
+// {
+// 	t_token		*redir_token;
+// 	t_token		*file_token;
+// 	t_node_type redir_type;
+// 	t_ast		*node;
+// 	int flag;
+// 	int fd;
+// 	char	*old_value;
+
+// 	flag = 0;
+// 	redir_token = advance(p);
+// 	redir_type = get_redir_type(redir_token->type);
+// 	fd = redir_token->fd;
+// 	// printf("-->fd %i",fd);
+// 	file_token = expect(p, T_WORD);
+// 	if (!file_token)
+// 	{
+// 		free_ast(cmd);
+// 		return (NULL);
+// 	}
+// 	if (redir_type == N_HEREDOC)
+// 	{
+// 		old_value = file_token->value;
+// 		file_token->value = strip_quotes(file_token->value, &flag);
+// 		free(old_value);
+// 		node = create_redir_node(redir_type, file_token->value, cmd, fd);
+// 		if (node)
+// 		{
+// 			node->heredoc_content = read_heredoc_content(file_token->value);
+// 			if (!flag)
+// 				node->heredoc_content = expand_heredoc_line(node->heredoc_content, shell);
+// 				// node->heredoc_content = expand_and_replace(&(node->heredoc_content), shell);
+// 		}
+// 		return (node);
+// 	}
+// 	return (create_redir_node(redir_type, file_token->value, cmd, fd));
+// }
+
 t_ast *parse_one_redir(t_parser *p, t_ast *cmd, t_shell *shell)
 {
-	t_token		*redir_token;
-	t_token		*file_token;
-	t_node_type redir_type;
-	t_ast		*node;
-	int flag;
-	int fd;
-	char	*old_value;
-
-	flag = 0;
-	redir_token = advance(p);
-	redir_type = get_redir_type(redir_token->type);
-	fd = redir_token->fd;
-	// printf("-->fd %i",fd);
-	file_token = expect(p, T_WORD);
-	if (!file_token)
-	{
-		free_ast(cmd);
-		return (NULL);
-	}
-	if (redir_type == N_HEREDOC)
-	{
-		old_value = file_token->value;
-		file_token->value = strip_quotes(file_token->value, &flag);
-		free(old_value);
-		node = create_redir_node(redir_type, file_token->value, cmd, fd);
-		if (node)
-		{
-			node->heredoc_content = read_heredoc_content(file_token->value);
-			if (!flag)
-				node->heredoc_content = expand_heredoc_line(node->heredoc_content, shell);
-				// node->heredoc_content = expand_and_replace(&(node->heredoc_content), shell);
-		}
-		return (node);
-	}
-	return (create_redir_node(redir_type, file_token->value, cmd, fd));
+    t_token     *redir_token;
+    t_token     *file_token;
+    t_node_type redir_type;
+    t_ast       *node;
+    int flag;
+    int fd;
+    char    *old_value;
+    pid_t   pid;
+    int     status = 0;
+    int     pipefd[2];
+    char    *heredoc_content;
+    char    buffer[4096];
+    ssize_t n;
+    char    *tmp;
+    struct sigaction    sa_old;
+    flag = 0;
+    if (g_sigint_received == 130)
+    {
+        free_ast(cmd);
+        return (NULL);
+    }
+    redir_token = advance(p);
+    redir_type = get_redir_type(redir_token->type);
+    fd = redir_token->fd;
+    file_token = expect(p, T_WORD);
+    if (!file_token)
+    {
+        free_ast(cmd);
+        return (NULL);
+    }
+    if (redir_type == N_HEREDOC)
+    {
+        old_value = file_token->value;
+        file_token->value = strip_quotes(file_token->value, &flag);
+        free(old_value);
+        node = create_redir_node(redir_type, file_token->value, cmd, fd);
+        if (node)
+        {
+            /* Fork to read heredoc */
+            if (pipe(pipefd) == -1)
+            {
+                free_ast(node);
+                return (NULL);
+            }
+            pid = fork();
+            if (pid == -1)
+            {
+                close(pipefd[0]);
+                close(pipefd[1]);
+                free_ast(node);
+                return (NULL);
+            }
+            if (pid == 0)  /* CHILD */
+            {
+                close(pipefd[0]);
+                char *content = read_heredoc_content(file_token->value);
+                if (content)
+                {
+                    write(pipefd[1], content, ft_strlen(content));
+                    free(content);
+                }
+                close(pipefd[1]);
+                exit(0);
+            }
+            /* PARENT */
+            close(pipefd[1]);
+            sigaction(SIGINT, NULL, &sa_old);  /* Save current handler */
+            signal(SIGINT, SIG_IGN);  /* Ignore while waiting */
+            waitpid(pid, &status, 0);
+            sigaction(SIGINT, &sa_old, NULL);  /* Restore saved handler */
+            /* If child was killed (Ctrl+C), return NULL */
+            if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
+            {
+                close(pipefd[0]);
+                free_ast(node);
+                g_sigint_received = 130;
+                return (NULL);
+            }
+            /* Read heredoc content from pipe */
+            heredoc_content = ft_strdup("");
+            while ((n = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0)
+            {
+                buffer[n] = '\0';
+                tmp = heredoc_content;
+                heredoc_content = ft_strjoin(heredoc_content, buffer);
+                free(tmp);
+            }
+            close(pipefd[0]);
+            node->heredoc_content = heredoc_content;
+            /* If heredoc reading was interrupted by signal, stop parsing */
+            if (!node->heredoc_content && g_sigint_received)
+            {
+                // free_ast(node);
+				cleanup_shell(shell);
+                return (NULL);
+            }
+            if (!flag && node->heredoc_content)
+                node->heredoc_content = expand_heredoc_line(node->heredoc_content, shell);
+        }
+        return (node);
+    }
+    return (create_redir_node(redir_type, file_token->value, cmd, fd));
 }
--- src/parser/parser_cmd_redirect_utils_new.c
+++ src/parser/parser_cmd_redirect_utils_new.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_cmd_redirect_utils_new.c                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksng <ksng@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: jotong <jotong@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 16:56:13 by ksng              #+#    #+#             */
-/*   Updated: 2025/11/21 13:58:37 by ksng             ###   ########.fr       */
+/*   Updated: 2026/01/07 21:44:17 by jotong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,28 +56,87 @@ int	count_words(t_parser *p)
 	return (count);
 }
 
-char	*read_heredoc_content(char *delimiter)
+static void sigint_heredoc(int sig, siginfo_t *info, void *context)
 {
-	char	*line;
-	char	*content;
-	char	*tmp;
+    (void)context;
+    (void)info;
+    // g_sigint_received = 130;
+    if (sig == SIGINT)
+    {
+        write(1, "\n", 1);
+        // rl_on_new_line();
+        // rl_replace_line("", 0);
+        // rl_redisplay();
+        g_sigint_received = 130;
+        close(STDIN_FILENO);
+    }
+}
 
-	content = ft_strdup("");
-	while (1)
-	{
-		line = readline("> ");
-		if (!line || ft_strcmp(line, delimiter) == 0)
-		{
-			free(line);
-			break ;
-		}
-		tmp = content;
-		content = ft_strjoin(content, line);
-		free(tmp);
-		tmp = content;
-		content = ft_strjoin(content, "\n");
-		free(tmp);
-		free(line);
-	}
-	return (content);
+// char	*read_heredoc_content(char *delimiter)
+// {
+// 	char	*line;
+// 	char	*content;
+// 	char	*tmp;
+
+// 	content = ft_strdup("");
+// 	while (1)
+// 	{
+// 		line = readline("> ");
+// 		if (!line || ft_strcmp(line, delimiter) == 0)
+// 		{
+// 			free(line);
+// 			break ;
+// 		}
+// 		tmp = content;
+// 		content = ft_strjoin(content, line);
+// 		free(tmp);
+// 		tmp = content;
+// 		content = ft_strjoin(content, "\n");
+// 		free(tmp);
+// 		free(line);
+// 	}
+// 	return (content);
+// }
+
+char    *read_heredoc_content(char *delimiter)
+{
+    char    *line;
+    char    *content;
+    char    *tmp;
+    struct sigaction    sa;
+    ft_bzero(&sa, sizeof(sa));
+    sa.sa_sigaction = sigint_heredoc;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    // signal(SIGQUIT, SIG_IGN);
+    if (sigaction(SIGINT, &sa, NULL) == -1)
+    {
+        perror("sigaction failed");
+        exit(EXIT_FAILURE);
+    }
+    content = ft_strdup("");
+    while (1)
+    {
+        line = readline("> ");
+         if (g_sigint_received == 130)
+        {
+            free(line);
+            free(content);
+            exit(1);  /* Exit with code 1 */
+        }
+        if (!line || ft_strcmp(line, delimiter) == 0)
+        {
+            free(line);
+            break ;
+        }
+        tmp = content;
+        content = ft_strjoin(content, line);
+        free(tmp);
+        tmp = content;
+        content = ft_strjoin(content, "\n");
+        free(tmp);
+        free(line);
+    }
+    // set_signals();
+    return (content);
 }
